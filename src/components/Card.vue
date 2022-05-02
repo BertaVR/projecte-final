c85b73a0e14ea01006dc21b0176d7222e29be178
BertaVR <bverges@cifpfbmoll.eu>
2022-05-02 19:13:14 +0200

    Intento de card pero no funciona

---
+++ src/components/Card.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card
+    :loading="loading"
+    class="mx-auto my-12"
+    max-width="374"
+  >
+    <template slot="progress">
+      <v-progress-linear
+        color="deep-purple"
+        height="10"
+        indeterminate
+      ></v-progress-linear>
+    </template>
+
+    <v-img
+      height="250"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+
+    <v-card-title>Nombre del objeto</v-card-title>
+
+    <v-card-text>
+
+<div id="price">
+      <div id="amount" class=" text-subtitle-1">
+          80
+      </div>
+        <div id="currency" class="text-subtitle-1">
+          €
+      </div>
+      </div>
+
+      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+    <v-card-actions>
+
+
+      <v-spacer></v-spacer>
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+
+</script>
+<style>
+#price{display: flex}
+</style>
