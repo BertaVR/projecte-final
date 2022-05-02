089e8c2900697a3dc5b3d3aaca89cd9b71e2128c
BertaVR <bverges@cifpfbmoll.eu>
2022-05-02 17:54:44 +0200

    vuetify instalado y header funcioan

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<Header></Header>
+</div>
 </template>
 
 <script>
+import Header from "../components/Header.vue";
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
-  },
+    Header
+},
 };
 </script>
