15158861104ac52ba48a9a4abfc7d03edc3c0fff
BertaVR <bverges@cifpfbmoll.eu>
2022-05-17 17:27:56 +0200

    feat: screen de login

---
+++ src/components/Login.vue
@@ -0,0 +1,48 @@
+
+<template>
+    <v-app>
+        <v-main>
+            <v-container fluid fill-height>
+                <v-layout align-center justify-center>
+                    <v-flex xs12 sm8 md4>
+                        <v-card class="elevation-12" >
+                            <v-toolbar  dark color="accent">
+                                <v-toolbar-title>Login form</v-toolbar-title>
+                            </v-toolbar>
+                            <v-card-text>
+                                <form ref="form" @submit.prevent="login()">
+                                    <v-text-field prepend-icon="person" v-model="username" name="username"
+                                        label="Username" type="text" placeholder="username" required></v-text-field>
+
+                                    <v-text-field prepend-icon="lock" v-model="password" name="password"
+                                        label="Password" type="password" placeholder="password" required></v-text-field>
+                                    <v-btn type="submit" class="mt-4" color="primary" value="log in">Login</v-btn>
+                                </form>
+                            </v-card-text>
+                        </v-card>
+
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: "Login",
+    data() {
+        return {
+            username: "",
+            password: "",
+        };
+    },
+    methods: {
+        login() {
+            const { username } = this;
+            console.log(username + "logged in")
+        },
+    },
+    
+};
+</script>
