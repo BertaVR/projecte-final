8eb4b55e35bbde86f539582816664aa32676f60e
BertaVR <bverges@cifpfbmoll.eu>
2022-05-17 17:28:20 +0200

    feat: login

---
+++ src/views/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="packsSCreen">
+<Header></Header>
+<Login></Login>
+</div>
+</template>
+
+<script>
+
+import Login from "../components/Login.vue"
+export default {
+  name: "PacksScreen",
+  components: {
+   Login
+},
+};
+</script>
+<style>
+Header:{
+  width: 100%;
+}
+</style>
