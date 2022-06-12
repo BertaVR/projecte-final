921cec1ab55f77530addd78c8078587aca239c81
BertaVR <bverges@cifpfbmoll.eu>
2022-06-12 21:41:48 +0200

    fix: arreglado header por completo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,14 @@
 <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
 <template>
+
   <div class="topnav" id="myTopnav">
     <a href="/">Packs</a>
     <a href="/about">Items</a>
     <Carrito></Carrito>
 
-    <a href="javascript:void(0);" class="icon" @click="myFunction()">
-      <i class="fa fa-bars"></i>
-    </a>
+
   </div>
+
 </template>
 <style>
 /* Add a black background color to the top navigation */
@@ -53,57 +53,22 @@
 .topnav .icon {
   display: none;
 }
-
-/* When the screen is less than 600 pixels wide, hide all links, except for the first one ("Home"). Show the link that contains should open and close the topnav (.icon) */
-@media screen and (max-width: 600px) {
-  .topnav a:not(:first-child) {
-    display: none;
-  }
-
-  .topnav a.icon {
-    float: right;
-    display: block;
-  }
-}
-
-/* The "responsive" class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens (display the links vertically instead of horizontally) */
-@media screen and (max-width: 600px) {
-  .topnav.responsive {
-    position: relative;
-  }
-
-  .topnav.responsive a.icon {
-    position: absolute;
-    right: 0;
-    top: 0;
-  }
-
-  .topnav.responsive a {
-    float: none;
-    display: block;
-    text-align: left;
-  }
-}
 </style>
 <script>
 import Carrito from './Carrito.vue';
 export default {
   data: () => ({}),
   mounted() { this.activarLink() },
+  computed: {},
   methods: {
+    isDesktop() {
+      return (document.documentElement.clientWidth > 992)
+    },
     activarLink() {
       let path = window.location.pathname
       document.querySelector(`a[href='${path}']`).classList.add("active")
     },
-    myFunction() {
-      var x = document.getElementById("myTopnav");
-      if (x.className === "topnav") {
-        x.className += " responsive";
-      }
-      else {
-        x.className = "topnav";
-      }
-    }
+
   },
   components: { Carrito }
 }
