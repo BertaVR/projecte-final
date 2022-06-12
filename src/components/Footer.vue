05621be777207cbad50538c5dfd96e6f37aac926
BertaVR <bverges@cifpfbmoll.eu>
2022-06-12 22:39:56 +0200

    borrado cófigo innecesario

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,6 @@
     <template>
       <v-footer  padless>
         <v-row justify="center" no-gutters>
-          <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">
-            {{ link }}
-          </v-btn>
           <v-col class="secondary py-4 text-center white--text" cols="12">
             {{ new Date().getFullYear() }} — <strong>Berta Vergés</strong>
           </v-col>
